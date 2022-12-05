d85b5ff3a0829ffb38364d5d6a3073e0d9ac99bb
ikema77 <brhoom.kh@gmail.com>
2022-12-05 07:54:24 +0300

    First submission for moveright

---
+++ moveright.py
@@ -0,0 +1,9 @@
+string = str(input())
+list1 = list(string)
+for i in range(len(string)-1,-1,-1):
+    if (i == 0 and list1[i]=='1'):
+        list1[i]='0'
+    else:
+        list1[i] = list1[i-1]
+str1 = ''.join(list1)
+print(str1)
