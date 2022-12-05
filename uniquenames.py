b33830c5bbc05af86e38684890e53ff123849662
ikema77 <brhoom.kh@gmail.com>
2022-12-05 07:13:48 +0300

    Solving uniquenames problem

---
+++ uniquenames.py
@@ -0,0 +1,24 @@
+num = int(input())
+
+names=[[[],[]] for _ in range(num)]
+for i in range (0,num):
+    a = str(input())
+    names[i]=a.split()
+
+flag1 = 0
+flag2 =0
+for i in range(num):
+    for j in range(num):
+        if (i!=j):
+            if names[i][0] == names[j][0]:
+                flag1 = 1
+            if names[i][0] == names[j][1]:
+                flag1 = 1
+            if names[i][1] == names[j][0]:
+                flag2 = 1
+            if names[i][1] == names[j][1]:
+                flag2 = 1
+    if (flag1== 1 and flag2 == 1):
+        print('No')
+        exit()
+print('Yes')
