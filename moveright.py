9f5ab29087a0928a5e21769f1c7853f926b37710
ikema77 <brhoom.kh@gmail.com>
2022-12-05 08:13:10 +0300

    Accepted solution

--- moveright.py
+++ moveright.py
@@ -1,7 +1,7 @@
 string = str(input())
 list1 = list(string)
 for i in range(len(string)-1,-1,-1):
-    if (i == 0 and list1[i]=='1'):
+    if (i == 0):
         list1[i]='0'
     else:
         list1[i] = list1[i-1]
