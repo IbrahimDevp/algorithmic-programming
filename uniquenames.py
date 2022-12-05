3168ea21a37d3916270f9f472bcadc2a3d15d9ba
ikema77 <brhoom.kh@gmail.com>
2022-12-05 07:22:59 +0300

    Saving lines

--- uniquenames.py
+++ uniquenames.py
@@ -5,18 +5,13 @@ for i in range (0,num):
     a = str(input())
     names[i]=a.split()
 
-flag1 = 0
-flag2 =0
+flag1,flag2 = 0,0
 for i in range(num):
     for j in range(num):
         if (i!=j):
-            if names[i][0] == names[j][0]:
+            if names[i][0] == names[j][0] or names[i][0] == names[j][1]:
                 flag1 = 1
-            if names[i][0] == names[j][1]:
-                flag1 = 1
-            if names[i][1] == names[j][0]:
-                flag2 = 1
-            if names[i][1] == names[j][1]:
+            if names[i][1] == names[j][0] or names[i][1] == names[j][1]:
                 flag2 = 1
     if (flag1== 1 and flag2 == 1):
         print('No')
