2629086d6974ad7a6bbb7db126277e7e493d3d50
ikema77 <brhoom.kh@gmail.com>
2022-12-06 20:09:51 +0300

    Apple solved

--- apple.py
+++ apple.py
@@ -1,10 +1,10 @@
 X,Y,N = map(int,input().split())
-result = 0
-for i in range(0,N):
-    for j in range(0,N):
-        total = i*X + j*(Y*3)
-        if((total%X + total%Y) == N):
-            print(i,j)
-            print(total)
-            exit()
-10%1
+answer = 100000
+for i in range(N+1):
+    for j in range(N+1):
+        total = i + (j*3)
+        if(total == N):
+            if(i*X+j*Y < answer):
+                answer = i*X+j*Y
+
+print(answer)
