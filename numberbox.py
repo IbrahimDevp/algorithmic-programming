f6b31ac7e44d5f6f6b420c3ab8108c92fc0d9502
ikema77 <brhoom.kh@gmail.com>
2022-12-07 09:11:17 +0300

    Solving numberbox problem

--- numberbox.py
+++ numberbox.py
@@ -1,24 +1,22 @@
-num = int(input())
+number = int(input())
 
-list1 = [[] for _ in range (num)]
-max = 0
-for i in range (num):
-    a = input()
-    list1[i] = a
-max = []
-locnum = 1000
-loc = []
-for _ in range (num):
-    maxnum = 0
-    for i in range(0,num):
-        for j in range(0,num):
-            if(maxnum < int(list1[i][j])):
-                    locnum1 = str(i)+str(j)
-                    print(maxnum)
-                    if (locnum1 not in loc):
-                        maxnum = int(list1[i][j])
-                        locnum = int(locnum1)
-    loc.append(locnum)
-    max.append(maxnum)
-print(loc)
-print(max)
+list1 = [list(map(int, list(input()))) for _ in range(number)]
+
+dx = [1, 0, -1, 0, -1, 1, 1,-1]
+dy = [0, 1, 0, -1, 1, -1, 1,-1]
+ans = 0
+
+for i in range(number):
+    for j in range(number):
+        for k in range(8):
+            cur = 0
+            x,y = i,j
+            for _ in range (number):
+                cur *= 10
+                cur += list1[x][y]
+                x += dx[k]+number
+                y += dy[k]+number
+                x%=number
+                y%=number
+            ans = max(ans,cur)
+print(ans)
