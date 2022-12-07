7c240dc9305f35a857b30cdd878b78c5e58e39f2
ikema77 <brhoom.kh@gmail.com>
2022-12-07 16:53:12 +0300

    Closet solution yet

--- linesensor.py
+++ linesensor.py
@@ -1,4 +1,17 @@
 H,W = map(int,input().split())
 
-list1 = list(input() for _ in range(W))
+list1 = [[] for _ in range(W)]
+
+for i in range(H):
+    s = str(input())
+    list1[i] = s
+
+ans = ''
+for i in range(W):
+    count = 0
+    for j in range(H):
+        if list1[j][i] == '#':
+            count +=1
+    ans += str(count) + ' '
+print(ans)
 
