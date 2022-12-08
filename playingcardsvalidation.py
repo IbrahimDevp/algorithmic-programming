a74a174f0de3a5fed57a97cdf0e338af0fbbfcb8
ikema77 <brhoom.kh@gmail.com>
2022-12-08 15:29:41 +0300

    Playing cards validation solved

--- playingcardsvalidation.py
+++ playingcardsvalidation.py
@@ -7,14 +7,21 @@ notfound = False
 for i in range(len(list1)):
     for j in range(i+1,len(list1)):
         if (list1[i] == list1[j]): notfound = True
-c1 = list(zip(*list1))
+c1 = [list(a) for a in zip(*list1)]
 w = ['H','D','C','S']
 w2 = ['A','2','3','4','5','6','7','8','9','T','J','Q','K']
-if 'H' and 'D' and 'C' and 'S' not in c1[0]:
+
+if all(x not in w for x in c1[0]):
     notfound = True
-if 'A' and '2' and '3' and '4' and '5' and '6' and '7' and '8' and '9' and 'T' and 'J' and 'Q' and 'K' not in c1[1]:
+if all(y not in w2 for y in c1[1]):
     notfound = True
 
+for element in c1[0]:
+    if element not in w:
+        notfound = True
+for element in c1[1]:
+    if element not in w2:
+        notfound = True
 if notfound:
     print('No')
 else:
