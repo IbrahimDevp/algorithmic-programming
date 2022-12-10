bbada74c2bb79d09efe1339b2537217dc99782d8
ikema77 <brhoom.kh@gmail.com>
2022-12-10 08:15:14 +0300

    Simplifying logic

--- Inverseprefixsum.py
+++ Inverseprefixsum.py
@@ -1,9 +1,6 @@
 n = int(input())
 arr = list(map(int, input().split()))
-arr2 = []
 
-for i in range(n):
-        if (i == 0): arr2.append(arr[i])
-        else: arr2.append(arr[i]-sum(arr2))
-
-print(*arr2)
+print(arr[0], end = ' ')
+for i in range(1,n):
+        print(arr[i]-arr[i-1],end = ' ')
