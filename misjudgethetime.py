713d56cf75e40c9c3d47469fb4808d95dd40c8b6
ikema77 <brhoom.kh@gmail.com>
2022-12-11 06:16:55 +0300

    Simplifying logic

--- misjudgethetime.py
+++ misjudgethetime.py
@@ -1,38 +1,17 @@
-h,m = map(int,input().split())
+H,M = map(int,input().split())
 
-if h < 10:
-    AC ='0'+str(h)
-else: AC = str(h)
-if m < 10:
-    BD = '0'+str(m)
-else: BD = str(m)
-
-check = True
-ans = ''
-nAC = int(AC[0]+BD[0])
-nBD = int(AC[1]+BD[1])
-while(check):
-    nAC = int(AC[0]+BD[0])
-    nBD = int(AC[1]+BD[1])
-    #print(nAC,nBD)
-    if nAC>23 or nBD>59:
-        m += 1
-        if m < 10:
-            BD = '0'+str(m)
-        elif m == 60:
-            m = 0
-            BD = '00'
-            h += 1
-            if h>23:
-                h = 0
-                AC = '00'
-            elif h<10:
-                AC ='0'+str(h)
-            else:
-                AC =str(h)
-        else:
-            BD = str(m)
-    else:
-        print(int(AC),int(BD))
-        check = False
+while True:
+    A,C=H//10,H%10
+    B,D=H//10,M%10
+    AC = int(str(A)+str(B))
+    BD = int(str(C)+str(D))
+    if 0 <= AC <= 23 and 0 <= BD <= 59:
+        break
+    M+=1
+    if M == 60:
+        H+=1
+        M=0
+    if H == 24:
+        H = 0
+print(H,M)
 
