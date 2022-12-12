ee0c8661cd48bac9abaf65b02c118d08d3a0ca61
ikema77 <brhoom.kh@gmail.com>
2022-12-12 07:04:17 +0300

    Broken rounding solved

--- brokenrounding.py
+++ brokenrounding.py
@@ -1,12 +1,6 @@
-import math
 X,K = map(int, input().split())
 
-if pow(10,K)%X == 0:
-    print(0)
-elif (pow(10,K)%X >= pow(10,12)):
-    Y = X%pow(10,K)
-    print(abs(Y-X))
-else:
-    Y = X%pow(10,K)
-    Y2 = abs(Y-pow(10,K))
-    print(Y2+X)
+for i in range(K+1):
+    
+    X = round(X + 0.5, -i)
+print(int(X))
