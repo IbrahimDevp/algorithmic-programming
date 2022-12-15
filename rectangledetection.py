ea08ebb4d6e0d72f33dd2b73ce0bbda0d5f728c2
ikema77 <brhoom.kh@gmail.com>
2022-12-15 06:54:06 +0300

    Optimizing the solution for the problem

--- rectangledetection.py
+++ rectangledetection.py
@@ -1,27 +1,9 @@
-L = []
-for _ in range(10):
-    s = str(input())
-    L.append(s)
-A,B,C,D = 0,0,0,0
-max = 0
-for i in range (10):
-    if '#' in L[i]:
-        if A == 0 and B == 0:
-            A += 1 +i
-            B += 1 +i
-        else:
-            B+=1
+L = [list(input()) for _ in range (10)]
+ans = []
 for i in range(10):
     for j in range(10):
-        if '#' in L[i][j]:
-            if C == 0 and D == 0:
-                C += 1 + j
-                D += 1 + j
-            else:
-                if j+1 > D:
-                    D = j+1
-                if j+1 < C:
-                    C = j + 1
+        if L[i][j] == '#':
+            ans.append((i+1,j+1))
 
-print(A,B)
-print(C,D)
+print(ans[0][0],ans[-1][0])
+print(ans[0][1],ans[-1][1])
