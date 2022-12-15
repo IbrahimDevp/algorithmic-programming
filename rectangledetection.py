6369db3ce9f49719d567ae734e6dac42879e1041
ikema77 <brhoom.kh@gmail.com>
2022-12-15 06:41:07 +0300

    Solution for rectangledetection.py

--- rectangledetection.py
+++ rectangledetection.py
@@ -3,6 +3,7 @@ for _ in range(10):
     s = str(input())
     L.append(s)
 A,B,C,D = 0,0,0,0
+max = 0
 for i in range (10):
     if '#' in L[i]:
         if A == 0 and B == 0:
@@ -10,3 +11,17 @@ for i in range (10):
             B += 1 +i
         else:
             B+=1
+for i in range(10):
+    for j in range(10):
+        if '#' in L[i][j]:
+            if C == 0 and D == 0:
+                C += 1 + j
+                D += 1 + j
+            else:
+                if j+1 > D:
+                    D = j+1
+                if j+1 < C:
+                    C = j + 1
+
+print(A,B)
+print(C,D)
