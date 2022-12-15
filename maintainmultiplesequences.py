69df9462cd88988a3c6794341c2a1f4426d5e847
ikema77 <brhoom.kh@gmail.com>
2022-12-15 06:09:14 +0300

    Promting user to insert s and t values

--- maintainmultiplesequences.py
+++ maintainmultiplesequences.py
@@ -1,4 +1,10 @@
 N,Q = map(int,input().split())
 L = [list(map(int, input().split()[1:])) for _ in range (N)]
 s,t=[],[]
+for _ in range(Q):
+    val1,val2=map(int, input().split())
+    s.append(val1)
+    t.append(val2)
+
 print(L)
+print(s,t)
