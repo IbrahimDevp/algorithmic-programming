ef1282174b58b71f9cd09b14f634bc9a667efe83
ikema77 <brhoom.kh@gmail.com>
2022-12-15 06:31:52 +0300

    Getting A and B values corretly

--- rectangledetection.py
+++ rectangledetection.py
@@ -2,5 +2,11 @@ L = []
 for _ in range(10):
     s = str(input())
     L.append(s)
-for i in range(10):
-    print(L[i])
+A,B,C,D = 0,0,0,0
+for i in range (10):
+    if '#' in L[i]:
+        if A == 0 and B == 0:
+            A += 1 +i
+            B += 1 +i
+        else:
+            B+=1
