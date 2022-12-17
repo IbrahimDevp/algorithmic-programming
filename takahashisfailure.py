e0aa3b889fe9577f36a9b949741463c23b9198e0
ikema77 <brhoom.kh@gmail.com>
2022-12-17 07:11:46 +0300

    Optimizing the code

--- takahashisfailure.py
+++ takahashisfailure.py
@@ -1,16 +1,11 @@
 N,K = map(int,input().split())
 A = list(map(int,input().split()))
 B = list(map(int,input().split()))
-chance = list()
-mxfixed = max(A)
-for i in range (len(A)):
-    mx = max(A)
-    if mxfixed == mx:
-        mxind = A.index(mx)
-        chance.append(mxind+1)
-        A[mxind] = -1
-for i in chance:
-    if i in B:
+
+mx = max(A)
+
+for i in B:
+    if A[i-1] == mx:
         print('Yes')
         exit()
 print('No')
