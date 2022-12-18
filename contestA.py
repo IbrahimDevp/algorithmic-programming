a9e72adbe1b52bba15cf6276bd117f03aadaab1d
ikema77 <brhoom.kh@gmail.com>
2022-12-18 09:46:43 +0300

    Simplifying logic

--- contestA.py
+++ contestA.py
@@ -1,30 +1,12 @@
-t = int(input())
-while(t):
-    ans =''
+for _ in range(int(input())):
     n = int(input())
-    a = list(input())
-    total = int(a[0])
-    for i in range(n-1):
-        if total + int(a[i+1]) == 0:
-            ans+='+'
-            total += int(a[i+1])
-            #print(total,int(a[i+1]),'1')
-        elif total - int(a[i+1]) == 0:
-            ans+='-'
-            total -= int(a[i+1])
-            #print(total,int(a[i+1]),'2')
-        elif total + int(a[i+1]) == 1:
-            ans+='+'
-            total += int(a[i+1])
-           # print(total,int(a[i+1]),'3')
-        elif total - int(a[i+1]) == 1:
-            ans+='-'
-            total -= int(a[i+1])
-            #print(total,int(a[i+1]),'4')
+    arr = input()
+    res = int(arr[0])
+    for i in range(1,n):
+        if res == 1 and arr[i] == '1':
+            print('-',end = '')
+            res = 0
         else:
-            ans+='-'
-            total -= int(a[i+1])
-        
-        
-    print(ans)
-    t-=1
+            print('+', end='')
+            res += int(arr[i])
+    print()
