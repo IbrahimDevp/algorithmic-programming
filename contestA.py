195cb914e025ada5044d2e4a556fba32f117f4ca
ikema77 <brhoom.kh@gmail.com>
2022-12-18 09:44:07 +0300

    Solution accepted

--- contestA.py
+++ contestA.py
@@ -24,38 +24,6 @@ while(t):
         else:
             ans+='-'
             total -= int(a[i+1])
-            
-           # print(total,int(a[i+1]),'5')
-        
-        #1-1 + 0 = 0
-        #1-1 + 1 = 0
-
-        #0-1 + 1 = 0
-        #0-1 + 0 = -1
-
-        #1+0 + 1 = 2
-        #1+0 + 0 = 1
-
-        #0-0 + 1 = 1
-        #0+0 + 1 = 1
-        #0+0 + 0 = 0
-        # if a[i]== '0' and a[i+1]== '0':
-        #     ans+='+'
-        # elif a[i]== '1' and a[i+1]== '0':
-        #     ans+='+'
-        #     total +=1
-        # elif a[i]== '0' and a[i+1]== '1':
-        #     print(total)
-        #     if total > 0:
-        #         ans+='-'
-        #         total -=1
-        #     else:
-        #         ans+='+'
-        #         total +=1
-        # elif a[i]=='1' and a[i+1]=='1':
-        #     ans+='-'
-        # else:
-        #     ans+='not'
         
         
     print(ans)
