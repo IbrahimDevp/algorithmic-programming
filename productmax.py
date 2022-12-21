0b140bfa18064f424d8e150f0b4fa78fe48a6286
ikema77 <brhoom.kh@gmail.com>
2022-12-21 13:34:52 +0300

    Solution for abc178_b

--- productmax.py
+++ productmax.py
@@ -1,6 +1,14 @@
 a,b,c,d = map(int,input().split())
 ans = list()
-for i in range(a,b+1):
-    for j in range(c,d+1):
-        ans.append(i*j)
+mx1 = max(a,b)
+mx2 = max(c,d)
+mn1 = min(a,b)
+mn2 = min(c,d)
+
+
+for _ in range (4):
+    ans.append(mx1*mx2)
+    ans.append(mx1*mn2)
+    ans.append(mn1*mx2)
+    ans.append(mn1*mn2)
 print(max(ans))
