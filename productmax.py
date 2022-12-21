325d7eaa98caec88cd7fb73fdaabe9718c1260e4
ikema77 <brhoom.kh@gmail.com>
2022-12-21 13:40:27 +0300

    Simplifying the logic

--- productmax.py
+++ productmax.py
@@ -1,14 +1,2 @@
 a,b,c,d = map(int,input().split())
-ans = list()
-mx1 = max(a,b)
-mx2 = max(c,d)
-mn1 = min(a,b)
-mn2 = min(c,d)
-
-
-for _ in range (4):
-    ans.append(mx1*mx2)
-    ans.append(mx1*mn2)
-    ans.append(mn1*mx2)
-    ans.append(mn1*mn2)
-print(max(ans))
+print(max(a*c,a*d,b*c,b*d))
