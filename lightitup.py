5cea5b04c326479040ed6d88490c6f61ab1afb6b
ikema77 <brhoom.kh@gmail.com>
2022-12-22 08:30:52 +0300

    Creating two problem files

---
+++ lightitup.py
@@ -0,0 +1,9 @@
+N,K = map(int,input().split())
+A = list (map(int,input().split()))
+X = list()
+Y = list()
+for _ in range(N):
+    a,b = map(int,input().split())
+    X.append(a)
+    Y.append(b)
+
