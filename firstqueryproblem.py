be01fb0cd6974bb46b528265aa545b76e9786f58
ikema77 <brhoom.kh@gmail.com>
2022-12-26 06:54:14 +0300

    Simplifying printing

--- firstqueryproblem.py
+++ firstqueryproblem.py
@@ -1,13 +1,9 @@
 N = int(input())
-
 A = list(map(int, input().split()))
 Q = int(input())
-ans = list()
 for _ in range(Q):
     query = list(map(int,input().split()))
     if query[0] == 2:
-        ans.append(A[query[1]-1])
+       print(A[query[1]-1])
     else:
         A[query[1]-1]=query[2]
-for i in range(len(ans)):
-    print(ans[i])
