0781b9e9fe3acbfa967835d8cbe91825cea731bb
ikema77 <brhoom.kh@gmail.com>
2022-12-28 08:51:14 +0300

    Solution for hydrate

---
+++ Hydrate.py
@@ -0,0 +1,12 @@
+A,B,C,D = map(int,input().split())
+red = 0
+operation = 0
+if B > C * D:
+    print(-1)
+    exit()
+else:
+    while A > D*red:
+        A += B
+        red +=C
+        operation +=1
+print(operation)
