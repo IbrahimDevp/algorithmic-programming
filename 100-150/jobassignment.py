7956ecb8004020b9b3b7f3911d4fd984ce4b6cc8
ikema77 <brhoom.kh@gmail.com>
2023-01-02 20:43:00 +0300

    Solution for jobassignment

--- 100-150/jobassignment.py
+++ 100-150/jobassignment.py
@@ -6,43 +6,9 @@ for _ in range(N):
     a,b = map(int,input().split())
     A.append(a)
     B.append(b)
-    total.append(a+b)
-shortest = min(total)
-ind = total.index(shortest)
-shA=A[ind]
-shB=B[ind]
-A.pop(ind)
-B.pop(ind)
-minn = min(A)
-minn2 = min(B)
-if shA < shB:
-    if minn2 <= shortest:
-        print(minn2)
-        exit()
-    else:
-        print(shortest)
-        exit()
-elif shA > shB:
-    if minn <= shortest:
-        print(minn)
-        exit()
-    else:
-        print(shortest)
-        exit()
-else:
-    if minn <= shortest:
-        if minn2 <= minn:
-            print(minn2)
-            exit()
-        else:
-            print(minn)
-            exit()
-    if minn2 <= shortest:
-        if minn <= minn2:
-            print(minn)
-            exit()
-        else:
-            print(minn2)
-            exit()
-print(shortest)
+res = 10000000000
+for i in range(N):
+    for j in range(N):
+        res = min(res, A[i] + B[j] if i==j else max(A[i],B[j]))
 
+print(res)
