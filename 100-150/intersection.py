8d6a12ca83900178e3bc74520b6d8e1884ca8d33
ikema77 <brhoom.kh@gmail.com>
2023-01-06 15:19:32 +0300

    Solution for intersection

---
+++ 100-150/intersection.py
@@ -0,0 +1,14 @@
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+ 
+maxx = max(A)
+minn = min (B)
+count = 0
+if maxx <= minn:
+    for _ in range(maxx,minn+1):
+        count+=1
+else:
+    print(count)
+    exit()
+print(count)
