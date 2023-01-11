dde9b26e2796ae9e8ca8fbcc2493569a236b0dae
ikema77 <brhoom.kh@gmail.com>
2023-01-11 19:28:54 +0300

    Simplifying the code

--- 150-200/gooddistance.py
+++ 150-200/gooddistance.py
@@ -1,6 +1,5 @@
 import math
-def iswhole(n):
-    return n%1 ==0
+
 N,D = map(int, input().split())
 count = 0
 X = list(list(map(int,input().split())) for _ in range(N))
@@ -9,6 +8,6 @@ for i in range (N):
         tot = 0
         for k in range(D):
             tot += pow(abs(X[i][k]-X[j][k]),2)
-        if iswhole(math.sqrt(tot)):
+        if math.sqrt(tot).is_integer():
             count+=1
 print(count)
