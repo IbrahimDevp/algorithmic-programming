cf3d7edd4c01b6876027d853b8872266d95c53ae
ikema77 <brhoom.kh@gmail.com>
2023-01-15 13:42:37 +0300

    Solution for stringrotation

--- 150-200/stringrotation.py
+++ 150-200/stringrotation.py
@@ -1,15 +1,10 @@
 S = str(input())
 T = str(input())
-f = [0 for _ in range(150)]
-f2 = [0 for _ in range(150)]
-if len(S) == len(T):
-    for i in range(len(S)):
-        f[ord(S[i])] +=1
-        f2[ord(T[i])] +=1
-    if f == f2:
+
+for i in range(len(S)):
+    word = S[-1]+S[:-1]
+    S = word
+    if (S == T):
         print('Yes')
-    else:
-        print('No')
-else:
-    print('No')
-    
+        exit()
+print('No')
