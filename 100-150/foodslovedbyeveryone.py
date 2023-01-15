6259151e4ee5c2377c410094010a0c33d54cf31a
ikema77 <brhoom.kh@gmail.com>
2023-01-15 16:38:52 +0300

    Optimal solution :)

--- 100-150/foodslovedbyeveryone.py
+++ 100-150/foodslovedbyeveryone.py
@@ -1,9 +1,5 @@
 N,M = map(int,input().split())
 
 A=list(list(map(int,input().split()))[1:] for _ in range(N))
-freq = list(0 for _ in range(31))
-for i in range(N):
-    for j in range(N):
-        if i+1 in A[j]:
-            freq[i+1] += 1
-print(freq.count(N))
+common_values = A[0].intersection(*A)
+print(len(common_values))
