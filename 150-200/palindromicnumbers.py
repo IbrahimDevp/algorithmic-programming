84473e6c685aaaad418cb2acad08228fb06f2f0f
ikema77 <brhoom.kh@gmail.com>
2023-01-16 09:28:39 +0300

    Simplifying the code :)

--- 150-200/palindromicnumbers.py
+++ 150-200/palindromicnumbers.py
@@ -1,12 +1,6 @@
 A,B = map(int, input().split())
-C = list(str(A))
-
 count = 0
 for i in range(A,B+1):
-    T1 = list(str(i))
-    T1.reverse()
-    T2 = str(T1)
-    T1.reverse()
-    if T2 == str(T1):
+    if str(i) == str(i)[::-1]:
         count+=1
 print(count)
