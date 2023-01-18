d73e99af208b70099a735e9fd7298052931e6c34
ikema77 <brhoom.kh@gmail.com>
2023-01-18 11:08:03 +0300

    Solution for somesums

---
+++ 150-200/somesums.py
@@ -0,0 +1,10 @@
+N,A,B = map(int, input().split())
+
+a = 220
+
+anslist=list()
+for i in range(1,N+1):
+        sumn = sum([int(j) for j in str(i)])
+        if sumn >= A and sumn <= B:
+            anslist.append(i)
+print(sum(anslist))
