906dbc173a0547eae0b4693e323cbd39ab3b54d1
ikema77 <brhoom.kh@gmail.com>
2023-01-19 07:08:11 +0300

    Simplifying the code

--- 200-250/palace.py
+++ 200-250/palace.py
@@ -1,12 +1,10 @@
 N = int(input())
 T,A = map(int,input().split())
 H = list(map(int,input().split()))
-D = list()
-for i in H:
-    D.append(T-i*0.006)
-ans = 100000000000
-D2=list()
-for i in range(len(D)):
-    ans = min(ans,abs(D[i]-A))
-    D2.append(ans)
-print(D2.index(ans)+1)
+cnt = 100000000000
+for i in range(N):
+    temp = T-H[i]*0.006
+    if abs(temp-A) < cnt:
+        ans = i + 1
+        cnt =abs(temp-A)
+print(ans)
