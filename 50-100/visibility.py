da149c827aa182def0f9c7f40bd00c22996ed7f1
ikema77 <brhoom.kh@gmail.com>
2023-01-26 08:59:06 +0300

    Solution for visilibity

--- 50-100/visibility.py
+++ 50-100/visibility.py
@@ -1,28 +1,26 @@
 H,W,X,Y = map(int,input().split())
 
 S = list(list(input()) for _ in range(H))
-X-=1
-Y-=1
-count=0
-for i in range (X,H-1):
-    if S[i][Y] == '.':
-        count+=1
-    else:
-        break
-for i in range (Y,W-1):
-    if S[X][i] == '.':
-        count+=1
-    else:
-        break
-for i in range (X,0,-1):
-    if S[i][Y] == '.':
-        count+=1
-    else:
-        break
-for i in range (Y,0,-1):
-    if S[X][i] == '.':
-        count+=1
-    else:
-        break
 
-print(count-2)
+ans=0
+for i in range(X-1, H):
+    if S[i][Y-1] == ".":
+        ans += 1
+    else:
+        break
+for i in range(X-1, -1, -1):
+    if S[i][Y-1] == ".":
+        ans += 1
+    else:
+        break
+for i in range(Y-1, W):
+    if S[X-1][i] == ".":
+        ans += 1
+    else:
+        break
+for i in range(Y-1, -1, -1):
+    if S[X-1][i] == ".":
+        ans += 1
+    else:
+        break
+print(ans-3)
