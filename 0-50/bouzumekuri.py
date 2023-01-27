809ab627bc6ce951431f68da570626eec7e4d588
ikema77 <brhoom.kh@gmail.com>
2023-01-27 21:45:00 +0300

    Simplifying the code

--- 0-50/bouzumekuri.py
+++ 0-50/bouzumekuri.py
@@ -1,10 +1,3 @@
 input()
-S = list(map(int,input()))
-
-for i in range(len(S)):
-    if S[i] == 1:
-        if i%2==0:
-            print('Takahashi')
-        else:
-            print('Aoki')
-        exit()
+s = input()
+print("Takahashi" if (s.find('1') % 2) == 0 else "Aoki")
