559084b5083adb69e61c19ea49aaf39db2d64545
ikema77 <brhoom.kh@gmail.com>
2023-01-28 13:58:12 +0300

    Solution for factorialyencoin with removing prints

--- 50-100/factorialyencoin.py
+++ 50-100/factorialyencoin.py
@@ -6,9 +6,7 @@ coins_used = 0
 
 for i in range(10, 0, -1):
     coin_value = factorial(i)
-    print(coin_value, end=' ')
     coins_used += P // coin_value
-    print(coins_used, end=' ')
-    P %= coin_value 
-    print(P)
+    P %= coin_value
+    
 print(coins_used)
