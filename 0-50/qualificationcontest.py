507640a78a57c2d831b54e8028dc2d1eab5b7cba
ikema77 <brhoom.kh@gmail.com>
2023-02-06 19:30:55 +0300

    Simplifying the code

--- 0-50/qualificationcontest.py
+++ 0-50/qualificationcontest.py
@@ -1,7 +1,3 @@
 N,K = map(int,input().split())
-names =list()
-for _ in range(K):
-    names.append(input())
-names.sort()
-for i in range(K):
-    print(names[i])
+names = [input() for _ in range (K)]
+print(*sorted(names), sep="\n")
