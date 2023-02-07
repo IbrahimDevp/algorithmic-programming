92aaaf2482140ad3e501e8f866807c77b487449d
ikema77 <brhoom.kh@gmail.com>
2023-02-07 23:49:43 +0300

    Failed submission

---
+++ 0-50/minimizeordering.py
@@ -0,0 +1,13 @@
+# from itertools import permutations
+# import re
+
+# word = input()
+# wordlist = permutations(word)
+# list2 = list()
+# for i in wordlist:
+#     list2.append(i)
+# list2.sort()
+# word2 = str(list2[0])
+# word2 = word2.strip()
+# word2 = re.sub('()''', '', word2)
+# print(word2)
