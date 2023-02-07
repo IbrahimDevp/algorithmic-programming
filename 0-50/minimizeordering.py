52216ed4314bde1be365b4152b654019e076a91b
ikema77 <brhoom.kh@gmail.com>
2023-02-08 00:07:38 +0300

    Solution for minimizerdering

--- 0-50/minimizeordering.py
+++ 0-50/minimizeordering.py
@@ -1,13 +1,9 @@
-# from itertools import permutations
-# import re
-
-# word = input()
-# wordlist = permutations(word)
-# list2 = list()
-# for i in wordlist:
-#     list2.append(i)
-# list2.sort()
-# word2 = str(list2[0])
-# word2 = word2.strip()
-# word2 = re.sub('()''', '', word2)
-# print(word2)
+word = list(input())
+word.sort()
+word2 = str(word)
+word2 = word2.replace(" ", "")
+word2 = word2.replace("[", "")
+word2 = word2.replace("]", "")
+word2 = word2.replace("'", "")
+word2 = word2.replace(",", "")
+print(word2)
