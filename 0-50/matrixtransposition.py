352d3e35f507c9e1f55d4bda3dfa19cb3fce374b
ikema77 <brhoom.kh@gmail.com>
2023-02-09 06:52:44 +0300

    Simplifying the code with the use of zip function

--- 0-50/matrixtransposition.py
+++ 0-50/matrixtransposition.py
@@ -1,6 +1,4 @@
 H,W = map(int,input().split())
 A =list(list(map(int,input().split())) for _ in range(H))
-for i in range(W):
-    for j in range(H):
-        print(A[j][i],end=" ")
-    print()
+for i in (zip(*A)):
+    print(*i)
