dc80220a09fdd08f501d25073f08c68ab6689187
ikema77 <brhoom.kh@gmail.com>
2023-02-21 17:43:59 +0300

    Solution for ordinarynumber

--- 50-100/ordinarynumber.py
+++ 50-100/ordinarynumber.py
@@ -1,8 +1,7 @@
-n = int(input())
-arr = list(map(int,input().split()))
-ordnum = list()
-for i in range(1,n-1):
-    arr2 = [arr[i-1],arr[i],arr[i+1]]
-    arr2.sort()
-    ordnum.append(arr2[1])
-print(len(set(ordnum)))
+N = int(input())
+p = list(map(int,input().split()))
+count = 0
+for i in range(1,N-1):
+    if p[i-1]< p[i]<p[i+1] or p[i-1]> p[i]>p[i+1]:
+        count+=1
+print(count)
