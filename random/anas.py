e3e3a6964f55a97ce5f8777346e421405cc7727f
ikema77 <brhoom.kh@gmail.com>
2023-03-28 20:14:57 +0300

    Solution for anas problem

---
+++ random/anas.py
@@ -0,0 +1,10 @@
+n,m = map(int,input().split())
+word = list(input())
+word2 = word.copy()
+for i in range(m):
+    for j in range(n-1):
+        if word[j]=='B' and word[j+1] =='G':
+            word2[j] = 'G'
+            word2[j+1] = 'B'
+    word = word2.copy()
+print(*word2, sep='')  
