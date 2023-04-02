0653668b404c3270f19febc5693669e73a900ca4
ikema77 <brhoom.kh@gmail.com>
2023-04-02 20:46:22 +0300

    Correct version

--- 450-500/scope.py
+++ 450-500/scope.py
@@ -2,29 +2,6 @@ word = input()
 word2 = []
 box = []
 count = 0
-
-for i in range(len(word)):
-    if word[i] == '(':
-        word2.append(word[i])
-        count=0
-    elif word[i] in box:
-        print('No')
-        exit()
-    elif word[i]==')':
-        word2.pop()
-        if count!=0:
-            del box[count*-1:]
-            count=0
-    else:
-        box.append(word[i])
-        count +=1
-print('Yes')
-
-
-word = input()
-word2 = []
-box = []
-count = 0
 for i in range(len(word)):
     if word[i] == '(':
         word2.append(word[i])
