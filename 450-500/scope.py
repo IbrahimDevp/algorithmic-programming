fd0d5c976a6c7c08a127a2d680538d3bca4d91a8
ikema77 <brhoom.kh@gmail.com>
2023-04-02 20:41:14 +0300

    Solution for scope

---
+++ 450-500/scope.py
@@ -0,0 +1,47 @@
+word = input()
+word2 = []
+box = []
+count = 0
+
+for i in range(len(word)):
+    if word[i] == '(':
+        word2.append(word[i])
+        count=0
+    elif word[i] in box:
+        print('No')
+        exit()
+    elif word[i]==')':
+        word2.pop()
+        if count!=0:
+            del box[count*-1:]
+            count=0
+    else:
+        box.append(word[i])
+        count +=1
+print('Yes')
+
+
+word = input()
+word2 = []
+box = []
+count = 0
+for i in range(len(word)):
+    if word[i] == '(':
+        word2.append(word[i])
+        count=0
+    elif word[i] in box:
+        print('No')
+        exit()
+    elif word[i] == ')' and word[i-1] =='(':
+        word2.pop()
+    elif word[i]==')':
+        word2.pop()
+        if count!=0:
+            del box[count*-1:]
+            count=0
+        else:
+            box.clear()
+    else:
+        box.append(word[i])
+        count+=1
+print('Yes')
